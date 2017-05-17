9b10cc1ad6df8a3415281e85194800d48bc14325
Tomas <tomika619@gmail.com>
2017-05-17 16:23:17 +0200

    Videos page
    Urobil som videos page , chyba tam menu a footer. Zakova neviem skade su
    tie linky na youtube, tie sa daju pouzit formou href , nedaju sa
    embeddovat do iframe lebo chyba ten tag. Ked je potrebne tak po ziskani
    ozajstnych linkov mozem prerobit na iframe, netrva dlho

--- src/contactPage/contactPage.php
+++ src/contactPage/contactPage.php
@@ -6,48 +6,53 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="all">
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<style>
+<style media="all">
     @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
-    
+    @media print {
+        body {
+            -webkit-print-color-adjust: exact;
+        }
+    }
+
 </style>
 </head>
 <body>
 <div class="container">
-    <div class="jumbotron jumbotron-sm" style="background-color:#4268f4;margin-top:2%;color:white;">
+    <div class="jumbotron jumbotron-sm" style="background-color:#4268f4 !important;margin-top:2%;color:white !important;">
         <div class="row">
             <div class="col-sm-12 col-lg-12">
-                <h1 class="h2" style="margin-top:-2%">
+                <h1 id="contact" class="h2" style="margin-top:-2%">
                     Kontakt
                 </h1>
             </div>
         </div>
     </div>
-</div>
-<div class="container">
+
+
     <div class="row">
         <div class="col-sm-6">
             <div class="well">
-                <h3 style="line-height:20%;"><i class="fa fa-home fa-1x" style="line-height:6%;color:#4268f4"></i> Adresa:</h3>
+                <h2 id = "adress" style="line-height:20%;"><i class="fa fa-home fa-1x" style="line-height:6%;color:#4268f4!important;"></i> Adresa:</h2>
                 <p>Ústav automobilovej mechatroniky, FEI STU, Ilkovičova 3,
                     812 19 Bratislava, Slovenská republika</p>
 
-                <h3><i class="fa fa-user fa-1x" style="line-height:6%;color:#4268f4"></i> Sekretariát:</h3>
+                <h2 id="secretary"><i class="fa fa-user fa-1x" style="line-height:6%;color:#4268f4!important;"></i> Sekretariát:</h2>
                 <p>Katarína Kermietová</p>
 
-                <h2><i class="fa fa-key fa-1x" style="line-height:6%;color:#4268f4"></i> Kancelária</h2>
+                <h3 id="kacelaria"><i class="fa fa-key fa-1x" style="line-height:6%;color:#4268f4"></i> Kancelária</h3>
                 <p>D116</p>
 
-                <h2><i class="fa fa-envelope fa-1x" style="line-height:6%;color:#4268f4"></i> E-mail:</h2>
+                <h3><i class="fa fa-envelope fa-1x" style="line-height:6%;color:#4268f4"></i> E-mail:</h3>
                 <p>katarina.kermietova[at]stuba.sk</p>
 
-                <h2><i class="fa fa-phone fa-1x" style="line-height:6%;color:#4268f4"></i> Telefónne číslo:</h2>
+                <h3 id="phoneNumber"><i class="fa fa-phone fa-1x" style="line-height:6%;color:#4268f4"></i> Telefónne číslo:</h3>
                 <p>+421 260 291 598</p>
                 <p>+421 265 429 734</p>
             </div>
---
+++ src/videos/videos.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head lang="sk">
+    <title>Videá</title>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="all">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style media="all">
+        #tabs .tab-content {
+            color : white;
+            background-color: #428bca;
+            padding : 5px 15px;
+        }
+
+        #tabs h3 {
+            color : white;
+            background-color: #428bca;
+            padding : 5px 15px;
+        }
+
+    </style>
+</head>
+<body>
+<?php
+include (__DIR__.'/../database/database.php');
+$ex = new Database();
+$js = $ex->fetchVideos();
+?>
+<div class="container">
+        <div class="jumbotron jumbotron-sm" style="background-color:#4268f4 !important;margin-top:2%;color:white !important;">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 id="videos" class="h2" style="margin-top:-2%">
+                    Videá
+                </h1>
+            </div>
+        </div>
+    </div>
+    <div id="tabs" class="col-sm-12">
+        <ul id="list" class="nav nav-tabs">
+            <li class="active" id="labak"><a  href="#1" data-toggle="tab" >Labák</a></li>
+            <li id="predmet"><a  href="#2" data-toggle="tab">Predmet</a></li>
+            <li id="prop"><a  href="#3" data-toggle="tab">Propagácia</a></li>
+            <!--<li id="zariadenie"><a  href="#1" data-toggle="tab">Zariadenie</a></li>-->
+        </ul>
+    </div>
+    <div class="tab-content ">
+        <div class="tab-pane active" id="1">
+            <?php
+           for($i=0;$i<count($js);$i++)
+           {
+               if($js[$i]["TYPE"]=="labák")
+               {
+                   echo "<h3>".$js[$i]["NAME"]."</h3><br>";
+                   echo "<a href='".$js[$i]['URL']."'>Link na video</a>";
+               }
+           }
+            ?>
+
+        </div>
+        <div class="tab-pane" id="2">
+            <?php
+            for($i=0;$i<count($js);$i++)
+            {
+            if($js[$i]["TYPE"]=="predmet")
+            {
+            echo "<h3>".$js[$i]["NAME"]."</h3><br>";
+            echo "<a href='".$js[$i]['URL']."'>Link na video</a>";
+            }
+            }
+            ?>
+        </div>
+        <div class="tab-pane" id="3">
+            <?php
+            for($i=0;$i<count($js);$i++)
+            {
+                if($js[$i]["TYPE"]=="propagácia")
+                {
+                    echo "<h3>".$js[$i]["NAME"]."</h3><br>";
+                    echo "<a href='".$js[$i]['URL']."'>Link na video</a>";
+                }
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+</div>
+
+</body>
+
+</html>
