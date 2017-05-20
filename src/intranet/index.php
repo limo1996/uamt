8d3e93ab1fbda9339f8727601ee6dbf9d3efb341
Matúš Lukáč <lukacmatusko@gmail.com>
2017-05-20 15:22:00 +0200

    #15 Intranet - Attendance - Repair my fail :D

--- src/intranet/index.php
+++ src/intranet/index.php
@@ -9,9 +9,7 @@
 
 
     <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-            crossorigin="anonymous"></script>
+
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/mainStylesIntranet.css" type="text/css" rel="stylesheet">
