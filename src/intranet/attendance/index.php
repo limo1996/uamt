b0f0cd3d5137380f3c1f6927856275138fb5f626
Matúš Lukáč <lukacmatusko@gmail.com>
2017-05-25 11:53:14 +0200

    Intranet-attendance menu & footer added

--- src/intranet/attendance/index.php
+++ src/intranet/attendance/index.php
@@ -1,6 +1,15 @@
 <?php
 require __DIR__.'/../../database/database.php';
 
+include('../../lang/langFunctions.php');
+$lang = 'sk';
+
+if (isset($_GET['lang']))
+    $lang = $_GET['lang'];
+
+$lan = new Text($lang);
+$text = $lan->getTextForPage('menu');
+
 session_start();
 
 if(!$_SESSION['user']){
@@ -17,17 +26,20 @@ if(!$_SESSION['user']){
     <link href="styles/MonthPicker.css" rel="stylesheet" type="text/css" />
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link href="../../css/mainStyles.css" type="text/css" rel="stylesheet">
+    <link href="../../menu/menuStyles.css" type="text/css" rel="stylesheet">
 
     <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
     <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-    <script src="https://cdn.rawgit.com/digitalBush/jquery.maskedinput/1.4.1/dist/jquery.maskedinput.min.js"></script>
     <script src="scripts/MonthPicker.js"></script>
     <script src="scripts/tableCreator.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link href="../../css/mainStylesIntranet.css" type="text/css" rel="stylesheet">
+    <link href="../../menu/menuStylesIntranet.css" type="text/css" rel="stylesheet">
+    <script src="../../menu/menuScripts.js"></script>
+
+    <style media="all">
+        @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
+    </style>
 </head>
 <body>
 <?php
@@ -44,18 +56,56 @@ $db->deleteEmployeeAbsence(date("Y-m-d", strtotime("2017-02-05")), 3, 3);
  * *****************/
 ?>
 
-<div class="jumbotron text-center" id="header">
-    <h1>Evidencia dochádzky</h1>
-    <p>Táto stránka umožnuje evidenciu dochádzky zamestnancov.</p>
-</div>
-<div class="container-fluid">
-    <div class="input-group input-group-lg col-sm-4" id="calendarWrapper">
-        <span class="input-group-addon fa fa-calendar"><img src="styles/calendar.png" width="21" height="21" alt="calendar"/></span>
-        <input id="NoIconDemo" type="text" class="form-control" aria-describedby="sizing-addon1"/>
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="menuBar">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
+                    class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span
+                    class="icon-bar"></span></button>
+        <p class="navbar-brand" style="color:purple;">UAMT - Intranet</p></div>
+    <div class="nav-flags">
+
     </div>
+    </div>
+    <div class="container">
+        <div class="navbar-header"></div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav" id="navMenu">
+                <li class="active"><a href="/uamt/intranet"><i class="fa fa-home fa-1x"></i></></a></li>
+                <li><a href="/uamt/intranet/pedagogika/index.php">Pedagogika</a></li>
+                <li><a href="/uamt/intranet/doktorandi/index.php">Doktorandi</a></li>
+                <li><a href="/uamt/intranet/publikacie/index.php">Publikácie</a></li>
+                <li><a href="/uamt/intranet/sluzobneCesty/index.php">Služobné cesty</a></li>
+                <li><a href="/uamt/intranet/nakupy/index.php">Nákupy</a></li>
+                <li><a href="/uamt/intranet/attendance/index.php">Dochádzka</a></li>
+                <li><a href="/uamt/intranet/rozdelenieUloh/index.php">Rozdelenie úloh</a></li>
+                <li><a href="/uamt/intranet/logout.php">Odhlásiť</a></li>
+                <li><a href="/uamt/" style="color:#0066cc"><i class="fa fa-flag fa-1x" style="color: #0066cc!important;"></i> Stránka</a></li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<div id="nazov">
+    <h2><?php echo "Evidencia dochádzky"; ?></h2>
+    <hr class="hr_nazov">
+</div>
+
+
+
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="input-group input-group-lg col-sm-4" id="calendarWrapper">
+                <input id="NoIconDemo" type="text" class="form-control" aria-describedby="sizing-addon1"/>
+            </div>
+        </div>
     <div class="text-right">
         <button type="button" class="btn btn-lg btn-primary" id="editBtn">   Edituj   </button>
     </div>
+    </div>
+
 </div>
 <br />
 <div class="text-center" id="selector">
@@ -109,14 +159,68 @@ $db->deleteEmployeeAbsence(date("Y-m-d", strtotime("2017-02-05")), 3, 3);
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
+<div class="row">
 
 <div class="col-sm-4">
-    <h2>Legenda</h2>
+    <h3>Legenda</h3>
     <span class="label label-primary">PN</span>
     <span class="label label-success">OČR</span>
     <span class="label label-info">Služobka</span>
     <span class="label label-warning">Dovolenka</span>
     <span class="label label-danger">Plán Dovolenky</span>
 </div>
+</div>
+<br>
+<footer>
+    <div class="container">
+        <div class="container">
+            <div class="col-sm-2 text-center">
+                <a href="http://is.stuba.sk/">AIS STU</a>
+            </div>
+            <div class="col-sm-2 text-center">
+                <a href="http://aladin.elf.stuba.sk/rozvrh/ ">Rozvrh hodín FEI</a>
+            </div>
+            <div class="col-sm-2 text-center">
+                <a href="http://elearn.elf.stuba.sk/moodle/  "> Moodle FEI</a>
+
+            </div>
+            <div class="col-sm-2 text-center">
+                <a href="https://www.jedalen.stuba.sk/WebKredit/"> Jedáleň STU </a>
+            </div>
+
+
+            <div class="col-sm-2 text-center">
+                <a href="https://www.facebook.com/UAMTFEISTU"> Facebook </a>
+            </div>
+            <div class="col-sm-2 text-center">
+                <a href="https://www.youtube.com/channel/UCo3WP2kC0AVpQMIiJR79TdA"> Youtube </a>
+            </div>
+        </div>
+        <hr>
+        <div class="container">
+
+            <div class="col-sm-4 text-center">
+                © Copyright 2017. Všetky práva vyhradené.
+            </div>
+            <div class="col-sm-4 text-center">
+                Baka | Lukac | Lichman | Valasik | Smetanka
+            </div>
+
+            <div class="col-sm-4 text-center">
+
+                <?php
+                if($lang == 'sk')
+                    echo "<a href='index.php?lang=sk' style='color: yellow' > Slovensky jazyk   | <a href='index.php?lang=en'>  English </a>";
+                else
+                    echo "<a href='index.php?lang=sk' > Slovensky jazyk   | <a href='index.php?lang=en'  style='    color: yellow'>  English </a>";
+
+                ?>
+            </div>
+
+        </div>
+
+    </div>
+</footer>
+
 
 </html>
